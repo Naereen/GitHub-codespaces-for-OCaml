b452a9a6f367cf1f97debb3dc40bf3a87ecf924c
Lilian Besson <Naereen@users.noreply.github.com>
2024-06-02 21:58:24 +0200

    Update hello.ml

--- hello/hello.ml
+++ hello/hello.ml
@@ -1,3 +1,14 @@
-open! Core
+let () = Printf.printf "Hello world from OCaml v4.14.0!\n";;
 
-let () = Stdio.print_endline "Hello World"
+(** Fonction factorielle : fact : int -> int. *)
+let rec fact n =
+  match n with
+  | 0 -> 1
+  | n when n < 0 -> 0
+  | _ -> n * (fact (n-1))
+;;
+
+(** Affichage de la fonction Factorielle. *)
+for n = 0 to 10 do
+  Printf.printf "fact %i = %i\n" n (fact n)
+done;;
